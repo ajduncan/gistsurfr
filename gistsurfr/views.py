7376994915ccde8447b90ff0a4fea624f1c018fe
Andy Duncan <andy@duncaningram.com>
2014-07-07 22:14:33 -0400

    Working authorization with random gist and some better styling and links in the template.

--- gistsurfr/views.py
+++ gistsurfr/views.py
@@ -1,6 +1,8 @@
 import os
+from random import randint
 
 from flask import Flask, g, redirect, render_template, render_template_string, request, send_from_directory, session, url_for
+from flask_github import GitHubError
 from flask_peewee.auth import Auth
 
 from gistsurfr.app import app, PEEWEE_DB
@@ -24,9 +26,16 @@ def after_request(response):
 
 @github.access_token_getter
 def token_getter():
-    user = auth.get_logged_in_user()
-    if user is not None:
-        return user.github_access_token
+	user_github = None
+	user = auth.get_logged_in_user()
+
+	if user is not None:
+		user_github = UserGithub.select().where(UserGithub.user == user).first()
+
+	if user_github is not None:
+		return user_github.github_access_token
+	else:
+		return None
 
 
 @app.route('/')
@@ -50,33 +59,57 @@ def authorized(access_token):
     if user is None:
     	user = User(username=str(access_token), password=str(access_token), email=str(access_token), admin=False, active=True)
     	user.save()
-    	github_user = UserGithub(User=user, github_access_token=access_token)
+    	github_user = UserGithub(user=user, github_access_token=access_token)
     	github_user.save()
-    	# this is hacky?
+    	# a more clever way than the above should be used perhaps.
     	auth.login_user(user)
-    	# user.username=str(github.get('user'))
-    	# user.save()
+    	gh_user = github.get('user')
+    	user.username=str(gh_user['login'])
+    	user.email=str(gh_user['email'])
+    	user.save()
     else:
     	# user_count = UserGithub.select().where((UserGithub.user == user) & (UserGithub.github_access_token == access_token)).count()
-    	github_user = UserGithub.select().where(UserGithub.user == user)
+    	github_user = UserGithub.select().where(UserGithub.user == user).first()
     	if github_user is None:
     		github_user = UserGithub(user=user, github_access_token=access_token)
     		github_user.save()
-    	else:
+
+    	if github_user.github_access_token != access_token:
     		github_user.github_access_token = access_token
     		github_user.save()
 
     return redirect(url_for('index'))
 
 
-@app.route('/github-authorize')
-def login():
+@app.route('/authorize')
+def authorize():
     user = auth.get_logged_in_user()
 
     if user is None:
         return github.authorize()
     else:
         return redirect(url_for('index'))
+
+
+@app.route('/account')
+@auth.login_required
+def account():
+	gh_user = github.get('user')
+	return render_template('gistsurfr/account.html', user=gh_user)
+
+
+@app.route('/random')
+@auth.login_required
+def random_gist():
+	while True:
+		try:
+			url = 'gists/{0}'.format(randint(1, 999999))
+			gist = github.get(url)
+			break
+		except GitHubError:
+			pass
+
+	return render_template('gistsurfr/random.html', gist=gist)
 
 
 @app.route('/favicon.ico')
