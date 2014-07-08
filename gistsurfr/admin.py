cb4e59e2ad52ff92c0d10231b3482dc5dc6d4b5e
Andy Duncan <andy@duncaningram.com>
2014-07-07 22:13:42 -0400

    Manage some more models in admin.

--- gistsurfr/admin.py
+++ gistsurfr/admin.py
@@ -2,9 +2,13 @@ from flask_peewee.admin import Admin
 
 from gistsurfr.app import app, db
 from gistsurfr.auth import auth
-from gistsurfr.models import User
+from gistsurfr.models import User, UserRelationship, UserGithub, UserGistFavorite
 
 admin = Admin(app, auth, branding='Gistsurfr')
 
 
 auth.register_admin(admin)
+
+admin.register(UserRelationship)
+admin.register(UserGithub)
+admin.register(UserGistFavorite)
